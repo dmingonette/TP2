98cd87a8788dd9eda8b844cc05bd0b65b2d766d2
dmingonette <diego_mingonette@hotmail.com>
2023-11-27 22:33:57 -0300

    crud api inventors

--- demos/servers/1-serverExpress.js
+++ demos/servers/1-serverExpress.js
@@ -5,23 +5,39 @@ const PORT = 3000;
 const app = express();
 
 const inventors = [
-  {first: 'Albert', last: 'Einstein', year: 1876},
-  {first: 'Issac', last: 'Newton', year: 1643},
-  {first: 'Galileo', last: 'Galilei', year: 1564},
-  {first: 'Marie', last: 'Curie', year: 1867},
-  {first: 'Johannes', last: 'Kepler', year: 1571},
-  {first: 'Nicolaus', last: 'Copernicus', year: 1473},
-  {first: 'Max', last: 'Planck', year: 1858}, 
+  {_id: 1,first: 'Albert', last: 'Einstein', year: 1876},
+  {_id: 2,first: 'Issac', last: 'Newton', year: 1643},
+  {_id: 3,first: 'Galileo', last: 'Galilei', year: 1564},
+  {_id: 4,first: 'Marie', last: 'Curie', year: 1867},
+  {_id: 5,first: 'Johannes', last: 'Kepler', year: 1571},
+  {_id: 6,first: 'Nicolaus', last: 'Copernicus', year: 1473},
+  {_id: 7,first: 'Max', last: 'Planck', year: 1858}, 
 ];
 
 app.get('/', (req, res) =>{
   res.send('Hola mundo express');
-})
+});
 
 app.get('/api/inventors', (req, res) =>{
+  const order = req.query.order;
+  console.log(order);
   res.json(inventors);
-})
+});
+
+app.get('/api/inventors/:id', (req, res) =>{
+  const id = parseInt(req.params.id);
+  res.json(inventors.filter(inventor => inventor._id === id));
+});
+
+app.use(express.json());
+
+app.post('/api/inventors', (req, res) => {
+  const inventorNuevo = req.body;
+  inventorNuevo._id = 8;
+  res.json(inventorNuevo)
+});
 
 app.listen(PORT,() =>{
   console.log(chalk.bgGreen(`Server express levantado en el puerto ${PORT}`));
-})
+});
+
